cb74ddfff3270bdb03fe737c6ad3b86f479e60d9
roynaldi19 <roynaldi19@gmail.com>
2022-01-31 09:11:41 +0700

    background dengan executor dan handler

--- app/src/main/java/com/roynaldi19/bfaa04_01backgroundthread/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/bfaa04_01backgroundthread/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.roynaldi19.bfaa04_01backgroundthread
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,21 +16,28 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
         btnStart.setOnClickListener {
-            try {
-                for (i in 0..10) {
-                    Thread.sleep(500)
-                    val percentage = i * 10
-                    if (percentage == 100) {
-                        tvStatus.setText(R.string.task_completed)
-                    } else {
-                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+            executor.execute {
+                try {
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post {
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text =
+                                    String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
                     }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
             }
         }
-
     }
 }
