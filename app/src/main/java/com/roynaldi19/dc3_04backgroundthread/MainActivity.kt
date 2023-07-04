6e9defdb9651d22fa90fec22f05918e26fef571c
roynaldi19 <roynaldi19@gmail.com>
2023-07-04 17:09:29 +0700

    update 23

--- app/src/main/java/com/roynaldi19/dc3_04backgroundthread/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc3_04backgroundthread/MainActivity.kt
@@ -1,24 +1,28 @@
 package com.roynaldi19.dc3_04backgroundthread
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.roynaldi19.dc3_04backgroundthread.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnStart = findViewById<Button>(R.id.btn_start)
-        val tvStatus = findViewById<TextView>(R.id.tv_status)
-
-        btnStart.setOnClickListener {
+        binding.btnStart.setOnClickListener {
             lifecycleScope.launch(Dispatchers.Default) {
                 for (i in 0..10) {
                     delay(500)
@@ -26,9 +30,9 @@ class MainActivity : AppCompatActivity() {
 
                     withContext(Dispatchers.Main) {
                         if (percentage == 100) {
-                            tvStatus.setText(R.string.task_completed)
+                            binding.tvStatus.setText(R.string.task_completed)
                         } else {
-                            tvStatus.text =
+                            binding.tvStatus.text =
                                 String.format(getString(R.string.compressing), percentage)
                         }
                     }
